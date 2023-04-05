070597f8807a2c0d2c26733d94f7b5a431d45dda
YuvalGu <yuval5659@gmail.com>
2023-04-05 12:57:22 +0300

    home page in progress - create add files buttons

--- main.py
+++ main.py
@@ -1,20 +1,15 @@
-from tkinter import *
+import tkinter as tk
 import customtkinter
+from views.home_page import HomePage
+from views.complex_example import AppExample
 
 
-class App(customtkinter.CTk):
+class App():
     def __init__(self):
-        super().__init__()
-        self.geometry("600x500")
-        self.title("Home page")
-
-        # add widgets to app
-        self.button = customtkinter.CTkButton(self, command=self.button_click)
-        self.button.grid(row=0, column=0, padx=20, pady=10)
-
-    # add methods to app
-    def button_click(self):
-        print("button click")
+        self.home_page = HomePage()
+        self.home_page.mainloop()
+        # self.example = AppExample()
+        # self.example.mainloop()
 
 
 app = App()
